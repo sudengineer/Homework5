f6da6291245850e6e17a9e454ff536756ffe280f
sudengineering <sudengineering@DESKTOP-L8ANE4A>
2021-01-23 20:42:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ayse");
+            myDictionary.Add(2,"Fatma");
+            myDictionary.Add(3, "Beyza");
+            myDictionary.DictionaryList();
+        }
+    }
+}
